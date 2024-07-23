5ec4c133e0d516ed00ecb9a337df58e6993c4d3b
T-Restrepo-A <tomas.res.ald@gmail.com>
2024-07-23 15:36:25 -0500

    Se hacen mas actualizaciones para empezar con el siguiente desarollo.

--- Proyecto_wiper/js/app.js
+++ Proyecto_wiper/js/app.js
@@ -28,6 +28,20 @@ window.addEventListener('load', function(){
 	});
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault();
+
+		const target = document.querySelector(this.getAttribute('href'));
+		if (target) {
+			window.scrollTo({
+				top: target.offsetTop,
+				behavior: 'smooth' // Transición suave
+			});
+		}
+	});
+});
+
 //-----------------------Codigo de login------------------------------
 
 const btnSignIn= document.getElementById("sign-in"),
